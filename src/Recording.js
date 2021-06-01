ce1b060eb3bbbe2d720fc49a22433c6a1f7d3bb7
Bruno Mendoza <hola@brunomendoza.es>
2021-06-01 04:06:10 +0100

    Recording component is capable of playing audios

--- src/Recording.js
+++ src/Recording.js
@@ -8,16 +8,32 @@ class Recording extends Component {
         }
     }
 
+    componentDidMount() {
+        let elem = document.getElementById("audio-recording")
+        
+        elem.addEventListener("play", ev => this.setState({isPlaying: true}))
+        elem.addEventListener("pause", ev => this.setState({isPlaying: false}))
+        elem.addEventListener("ended", ev => this.setState({isPlaying: false}))
+    }
+
     handleClick = event => {
-        console.log(this.state.isPlaying)
-        this.setState(state => ({isPlaying: !state.isPlaying}))
+        let elem = document.getElementById("audio-recording")
+
+        if(this.state.isPlaying) {
+            console.log("pausing...")
+            elem.pause()
+        } else {
+            console.log("playing...")
+            elem.load()
+            elem.play()
+        }
     }
 
     render() {
         return (
             <div className="clue__speaker">
                 <span onClick={this.handleClick} className="material-icons">volume_up</span>
-                <audio src={this.props.url}>
+                <audio id="audio-recording" src={this.props.url}>
                     Your browser does not support <code>audio</code> element
                 </audio>
             </div>
