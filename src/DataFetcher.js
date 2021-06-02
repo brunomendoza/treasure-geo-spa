23e038da4e118cac154b01da25cc417ef1ff2174
Bruno Mendoza <hola@brunomendoza.es>
2021-06-02 18:27:22 +0100

    Trying to control when the application should finish

--- src/DataFetcher.js
+++ src/DataFetcher.js
@@ -5,6 +5,7 @@ class DataFetcher extends Component {
         super(props)
         this.state = {
             isLoading: false,
+            isFinished: false,
             clue: null,
             error: null,
             id: 0
@@ -34,33 +35,40 @@ class DataFetcher extends Component {
 
     handleClick = (event) => {
         this.setState({isLoading: true})
-        this.setState(state => ({
-            id: state.id + 1
-        }))
 
-        fetch(this.props.url)
-            .then(res => res.json())
-            .then(clues => {
-                for (const clue of clues) {
-                    if (clue.id === this.state.id) {
-                        this.setState({
-                            isLoading: false,
-                            clue
-                        })
-                    }
-                }
-            })
-            .catch(error => this.setState({
-                isLoading: false,
-                error
-            }))
+        if (!this.state.isFinished) {
+            if (this.state > 4) {
+                this.setState({isFinished: true})
+            } else {
+                this.setState(state => ({
+                    id: state.id + 1
+                }))
+        
+                fetch(this.props.url)
+                    .then(res => res.json())
+                    .then(clues => {
+                        for (const clue of clues) {
+                            if (clue.id === this.state.id) {
+                                this.setState({
+                                    isLoading: false,
+                                    clue
+                                })
+                            }
+                        }
+                    })
+                    .catch(error => this.setState({
+                        isLoading: false,
+                        error
+                    }))
+            }
+        }
     }
 
     render() {
         const {isLoading, clue} = this.state
 
         return (
-            this.props.render(isLoading, clue, this.handleClick)
+            this.state.isFinished ? <h1>Done</h1> : this.props.render(isLoading, clue, this.handleClick)
         )
     }
 }
